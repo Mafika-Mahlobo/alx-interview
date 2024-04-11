03ae3c890fd636c9b35a96cc9d2e9074b2d230e6
Mafika <rudolphmafika@gmail.com>
2024-04-11 02:13:35 -0700

    Taks0, pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Implementing pascals triangle.
+"""
+
+def pascal_triangle(n):
+    """
+    function to implement a pascals triangle.
+
+    Args:
+        n (int): integer input
+    Returns:
+        List: a list of lists
+    """
+
+    pascal = []
+
+    if n <= 0:
+        return []
+
+    for i in range(n):
+
+        row = [1] * (i + 1)
+
+        for j in range(1, i):
+
+            row[j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
+
+        pascal.append(row)
+
+    return pascal
