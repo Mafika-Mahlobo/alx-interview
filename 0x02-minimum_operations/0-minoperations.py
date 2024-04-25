a396a99a99c817eea301a9d24a19b65f6f544080
Mafika <rudolphmafika@gmail.com>
2024-04-25 10:26:27 -0700

    Initial commit

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Solving Minimum operation problem
+"""
+
+def minOperations(n):
+    """
+    Check the minimum number of times to get Hxn with copy-all and paste
+
+    Args:
+        n (int): number of desired 'H's
+
+    Returns:
+        Int: number of copy/paste operation needed
+    """
+    pass
