b915cea27bf78eea135a09dbfeba47687b7335c5
Mafika <rudolphmafika@gmail.com>
2024-04-25 17:25:17 -0700

    answer

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -14,4 +14,22 @@ def minOperations(n):
     Returns:
         Int: number of copy/paste operation needed
     """
-    pass
+    
+    number_of_operations = 0
+    current_h = 1
+    clipboard = 1
+
+
+    while current_h < n:
+
+        if n % current_h == 0:
+            clipboard = current_h
+            number_of_operations += 2
+            current_h += clipboard
+        else:
+            number_of_operations += 1
+            current_h += clipboard
+
+    if current_h == n:
+        return number_of_operations
+    return 0
