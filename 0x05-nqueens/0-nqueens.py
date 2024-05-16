508c3a47c646b380ded73393fa90937ac3b12375
Mafika <rudolphmafika@gmail.com>
2024-05-16 02:31:25 -0700

    cleanup

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,62 +5,7 @@ N Queens algorithm
 """
 import sys
 
-if not len(sys.argv) == 2:
-  print("Usage: nqueens N")
-  sys.exit(1)
-
-try:
-  N = int(sys.argv[1])
-  if N < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-
-  solutions = nqueens(N)
-  print(solutions)
-
-except:
-  print("Something went wrong!")
-  sys.exit(1)
-
-
-def nqueens(N):
-    """
-    Implements N-queens algorithm.
-
-    Args:
-        N (int): Number of queens and board size.
-
-    Returns:
-        list: A list of lists representing valid board configurations.
-    """
-
-    solutions = []
-    formatted_solution = []
-    row = 0
-    col = 0
-    queen_row = 0
-    queen_col = 0
-    stack = [(0, [])]
-
-    while stack:
-        row, board = stack.pop()
-        if row == N:
-            for i in range(len(board)):
-                queen_row = i
-                queen_col = board[i]
-                formatted_solution.append([queen_row, queen_col])
-            solutions.append(formatted_solution)
-            continue
-
-        for col in range(N):
-            if is_safe(board, row, col):
-                stack.append((row + 1, board + [col]))
-    return solutions
-
-
-
 def is_safe(board, row, col):
-    
     """
     Checks if a queen can be safely placed at a specified position.
 
@@ -77,3 +22,51 @@ def is_safe(board, row, col):
         if board[i] == col or abs(board[i] - col) == abs(i - row):
             return False
     return True
+
+
+def nqueens(N):
+    """
+    Implements N-queens algorithm.
+
+    Args:
+        N (int): Number of queens and board size.
+
+    Returns:
+        list: A list of lists representing valid board configurations.
+    """
+
+    solutions = []
+    stack = [(0, [])]
+
+    while stack:
+        row, board = stack.pop()
+
+        if row == N:
+            formatted_solution = [[r, c] for r, c in enumerate(board)]
+        solutions.append(formatted_solution)
+        continue
+
+        for col in range(N):
+            if is_safe(board, row, col):
+        stack.append((row + 1, board + [col]))
+
+    return solutions
+
+
+if not len(sys.argv) == 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+try:
+    N = int(sys.argv[1])
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = nqueens(N)
+    for i in solutions:
+        print(i)
+
+except Exception as e:
+    print("N must be a number")
+    sys.exit(1)
