b6a9dc333e2c7b39a14f992cc51f3ce87c743bad
Mafika <rudolphmafika@gmail.com>
2024-07-11 05:51:16 -0700

    error fix. semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,30 +1,29 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 
 if (process.argv.length === 3) {
-   
-   const movieId = parseInt(process.argv[2])
-   const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+  const movieId = parseInt(process.argv[2]);
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-   request(url, (error, res, body) => {
-      if (error) {
-         console.error("Error:", error);
-      } else {
-         const data = JSON.parse(body);
-	 const characters = data.characters
-	 for (i = 0; i < characters.length; i++) {
-            request(characters[i], (error, res, body) => {
-               if (error) {
-                 console.error("Error:", error);
-	       } else {
-		  const data = JSON.parse(body);
-                  console.log(data.name);
-	       }
-	    });
-	 }
-     }
-   });
+  request(url, (error, res, body) => {
+    if (error) {
+      console.error('Error:', error);
+    } else {
+      const data = JSON.parse(body);
+      const characters = data.characters;
+      for (let i = 0; i < characters.length; i++) {
+        request(characters[i], (error, res, body) => {
+          if (error) {
+            console.error('Error:', error);
+          } else {
+            const data = JSON.parse(body);
+            console.log(data.name);
+          }
+        });
+      }
+    }
+  });
 } else {
-   console.log("No arguments found!");
+  console.log('No arguments found!');
 }
