7201c59a26d80da1a8b9371161490eeb7a210da3
Mafika <rudolphmafika@gmail.com>
2024-08-08 09:49:42 -0700

    Initial commit

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pytho3
+
+"""
+Prime Game implementation
+"""
+
+
+def isWinner(x, nums):
+
+    """
+    Returns name of player to most rounds of prime game (Ben or Maria)
+    """
+
+    pass
